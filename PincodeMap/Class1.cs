d71ca7e3b04f6add2ea715009bc96c046c8275ad
Sandeep <lagishetty.sandeep@gmail.com>
2015-10-29 11:33:02 +0530

    Completed

---
+++ PincodeMap/Class1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PincodeMap
+{
+    class Class1
+    {
+
+public class Pincode
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class OfficeType
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class Deliverystatus
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class Divisionname
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class Regionname
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class Circlename
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class Taluk
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class Districtname
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class Statename
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class Telephone
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class RelatedSuboffice
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class RelatedHeadoffice
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class Longitude
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class Latitude
+{
+    public string type { get; set; }
+    public string size { get; set; }
+    public string length { get; set; }
+    public bool __invalid_name__not_null { get; set; }
+    public string description { get; set; }
+}
+
+public class Fields
+{
+    
+    public Pincode pincode { get; set; }
+    public OfficeType officeType { get; set; }
+    public Deliverystatus Deliverystatus { get; set; }
+    public Divisionname divisionname { get; set; }
+    public Regionname regionname { get; set; }
+    public Circlename circlename { get; set; }
+    public Taluk Taluk { get; set; }
+    public Districtname Districtname { get; set; }
+    public Statename statename { get; set; }
+    public Telephone Telephone { get; set; }
+    public RelatedSuboffice __invalid_name__Related_Suboffice { get; set; }
+    public RelatedHeadoffice __invalid_name__Related_Headoffice { get; set; }
+    public Longitude longitude { get; set; }
+    public Latitude latitude { get; set; }
+}
+
+public class Record
+{
+    
+    public string pincode { get; set; }
+    public string officeType { get; set; }
+    public string Deliverystatus { get; set; }
+    public string divisionname { get; set; }
+    public string regionname { get; set; }
+    public string circlename { get; set; }
+    public string Taluk { get; set; }
+    public string Districtname { get; set; }
+    public string statename { get; set; }
+    public string Telephone { get; set; }
+    public string __invalid_name__Related_Suboffice { get; set; }
+    public string __invalid_name__Related_Headoffice { get; set; }
+    public string longitude { get; set; }
+    public string latitude { get; set; }
+}
+
+public class RootObject
+{
+    public string help { get; set; }
+    public bool success { get; set; }
+    public int count { get; set; }
+    public Fields fields { get; set; }
+    public List<Record> records { get; set; }
+}
+
+
+    }
+}
---
+++ PincodeMap/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Bing.Maps;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PincodeMap
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        public class MyText
+        {
+            public string t1 { get; set; }
+            public string t2 { get; set; }
+        }
+
+        private async void search_button_Click(object sender, RoutedEventArgs e)
+        {
+            map1.Credentials = "AutVzTJ62c1kL9L5ni3TBhm5nr1wGSSxL04Zrsw6DTTUeoTT7VqS1Vup4vFmSYaL";
+            Uri geocodeRequest = new Uri(string.Format("https://data.gov.in/api/datastore/resource.json?resource_id=04cbe4b1-2f2b-4c39-a1d5-1c2e28bc0e32&api-key=89e479d24bcb37745046aa07c9a39f4b&filters[pincode]={0}", text_pin.Text)); // 89e479d24bcb37745046aa07c9a39f4b
+            
+            HttpClient client = new HttpClient();
+            
+            HttpResponseMessage response = await client.GetAsync(geocodeRequest);
+            if (response.IsSuccessStatusCode == true)
+            {
+                var data = response.Content.ReadAsStringAsync();
+                var mylist = JsonConvert.DeserializeObject<PincodeMap.Class1.RootObject>(data.Result);
+
+
+                var mydataa = mylist.records;
+
+                var record_data = mydataa[0];
+
+                var mylati = record_data.latitude;
+                var mylongi = record_data.longitude;
+                
+
+                
+                
+
+                MyText mt = new MyText();
+                mt.t1 = record_data.Taluk;
+                
+                this.DataContext = mt;
+
+
+                   
+
+                    Location l = new Location();
+
+                if(mylati.Length>2)
+                {
+                    l.Latitude = Convert.ToDouble(mylati);
+                    l.Longitude = Convert.ToDouble(mylongi);
+                    map1.SetView(l, 15);
+                    MapLayer.SetPosition(pp, l);
+                }
+                else
+                {
+                    var msd = new MessageDialog("No Latitude and Longitude values").ShowAsync();
+                }
+                    
+                
+            }
+        }
+    }
+}
